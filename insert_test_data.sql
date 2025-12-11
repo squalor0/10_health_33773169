dace88de8867cd28413ef23e6eb0609d146d1848
Blake Murphy <bmurp001@gold.ac.uk>
2025-12-11 22:20:47 +0000

    wrong password

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -3,5 +3,5 @@ USE health;
 INSERT INTO users (username, password_hash)
 VALUES (
     'gold',
-    '$2b$10$qDq22f8vPqOKrX.uoHgiHOcCfAmCFMre7oVYeZiDOSWJozo/3qcdi'
+    '$2b$10$ITT4cDBSeFBGn8dth8N2O.UHhoO4sXBCq2RPBX6pT3p8KdgMnkC4O'
 );
